2fa22eae25ff31ce1c2a9fea5d362172c86b4f6c
Shaho Bashoki <bashoki.shaho@gmail.com>
2024-03-28 12:55:23 +0330

    feat: add env variables (#2)
    issue #1

---
+++ env.example.js
@@ -0,0 +1,9 @@
+var envExample = {
+  // GOOGLE
+  GOOGLE_API_KEY: "api_key",
+  GOOGLE_CALENDAR_CALENDARS_URL:
+    "https://www.googleapis.com/calendar/v3/calendars",
+  GOOGLE_CLIENT_ID: "client_id",
+  GOOGLE_PEOPLE_ME_CONNECTIONS_URL:
+    "https://people.googleapis.com/v1/people/me/connections",
+};
